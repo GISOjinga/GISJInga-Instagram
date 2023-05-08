6a0f29ab3a17ee5368098f219ffb3ef9ffe27c52
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-05-08 19:56:58 -0400

    Update 2

---
+++ src/Other.js
@@ -0,0 +1,13 @@
+
+
+
+// opens create post window
+let openCreatePostPrompt = () => {
+    document.getElementById("NewPost").style.display = "flex";
+};
+
+let closeCreatePostPrompt = () => {
+    document.getElementById("NewPost").style.display = "none";
+};
+
+
